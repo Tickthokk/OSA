250a08e5575d15c9bb73ade2105decbb730a27d4
Tickthokk <tickthokk@gmail.com>
2012-11-21 09:33:02 -0500

    QA DB changes

--- application/config/database.php
+++ application/config/database.php
@@ -80,7 +80,14 @@ $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
 if ($active_group == 'qa')
-	include 'qa/database.php';
+{
+	// QA is largely the same as default
+	$db['qa'] = $db['default'];
+	// The following are the difference
+	$db['qa']['hostname'] = DB_HOST;
+	$db['qa']['username'] = DB_USER;
+	$db['qa']['password'] = DB_PASSWORD;
+}
 
 if ($active_group == 'production')
 	include 'production/database.php';
