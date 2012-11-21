df836a66a1d8eb2517d99fd77d988acfb7e30013
Tickthokk <tickthokk@gmail.com>
2012-11-21 09:57:37 -0500

    Removed QA Folder

--- application/config/qa/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// QA is largely the same as default
-$db['qa'] = $db['default'];
-// The following are the difference
-$db['qa']['hostname'] = DB_HOST;
-$db['qa']['username'] = DB_USER;
-$db['qa']['password'] = DB_PASSWORD;
